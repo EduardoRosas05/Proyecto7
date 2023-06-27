be79a1f119324a47240542f2573c552f1c6cfbdf
EduardoRosas05 <rosasvega56@gmail.com>
2023-06-27 14:13:56 -0600

    Corrección en base de datos

--- database/migrations/20230627195244-create-income.js
+++ database/migrations/20230627195244-create-income.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('incomes', {
+    await queryInterface.createTable('Incomes', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -10,16 +10,13 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       acount: {
-        type: Sequelize.STRING,
-        allowNull: false
+        type: Sequelize.STRING
       },
       description: {
-        type: Sequelize.STRING,
-        allowNull: false
+        type: Sequelize.STRING
       },
       category: {
-        type: Sequelize.STRING,
-        allowNull: false
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
@@ -32,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('incomes');
+    await queryInterface.dropTable('Incomes');
   }
 };
--- database/models/index.js
+++ database/models/index.js
@@ -10,23 +10,39 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-import income from './income';
 
+import income from './income';
 
 db.Income = income(sequelize, Sequelize.DataTypes);
 
+
+/*
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+*/
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
-}); 
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
