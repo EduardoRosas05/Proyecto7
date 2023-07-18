bb889ce99f3161bf8c41c99d33bb1af3fb488172
Ruthisa23 <ChabelitaIsabelserrano@outlook.es>
2023-07-18 09:57:27 -0500

    Rama test incomes

--- database/models/income.js
+++ database/models/income.js
@@ -4,23 +4,53 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Income extends Model {
-    
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-      
-
-      models.Income.belongsTo(models.Category,
-        {
-          as: 'incomes',
-          foreignKey: 'categoryId'
-        }); 
-
+      // define association here
     }
   }
   Income.init({
-    description: DataTypes.STRING,
-    acount: DataTypes.STRING,
-    balance: DataTypes.STRING,
-    categoryId: DataTypes.INTEGER,
+    description: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        notNull:{
+          msg: 'la descripción es obligatoria'
+        },
+        isAlpha:{
+          msg: 'La descripción debe contener solo letras'
+        },
+      }
+    }, 
+    acount: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        notNull:{
+          msg: 'la cuenta es obligatoria'
+        },
+        isNumeric: {
+          msg: 'ingresa solo numeros'
+        }
+      }
+    },
+    balance: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        notNull:{
+          msg: 'el balance es obligatorio'
+        },
+        isNumeric: {
+          msg: 'ingresa solo numeros'
+        }
+      }
+    },
+    categotyId: DataTypes.INTEGER,
     usersId: DataTypes.INTEGER
   }, {
     sequelize,
---
+++ src/test/index.js
@@ -0,0 +1,4 @@
+//require('./calc_test');
+//require('./login_test');
+//require('./customers_test');
+require('./incomes_test');
