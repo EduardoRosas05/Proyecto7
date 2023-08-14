ba4b72c9a23747ada859ab1b95467474d1087d80
EduardoRosas05 <rosasvega56@gmail.com>
2023-08-14 13:47:37 -0600

    Funciones en la nube

--- database/models/index.js
+++ database/models/index.js
@@ -7,6 +7,9 @@ const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
+  if(config.dialect === "mysql"){
+    config.dialectModule = mysql12
+  }
 const db = {};
 
 let sequelize;
